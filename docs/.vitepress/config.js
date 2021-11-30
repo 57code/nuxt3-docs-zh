7dba77a0eb1138226ad213c8cdbb573ea501c299
57code <yt0379@sohu.com>
2021-12-01 00:18:18 +0800

    菜单

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  title: 'Nuxt3中文文档',
-  description: '由Nuxt3对赌学习群产出的中文文档'
-}
+  title: "Nuxt3中文文档",
+  description: "由Nuxt3对赌学习群产出的中文文档",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "使用",
+        link: "/",
+        children: [
+          { text: "数据获取", link: "/usage/data-fetching" }
+        ],
+      },
+    ],
+  },
+};
