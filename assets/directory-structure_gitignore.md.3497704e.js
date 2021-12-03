f5234d7eaa0691314a6a7df68b272790811ffea3
whylost <>
2021-12-03 04:57:49 +0000

    Deploying to gh-pages from @ 57code/nuxt3-docs-zh@ae094f6509bd92bffe6c31a557d0c9db6d8a986d 🚀

--- assets/directory-structure_gitignore.md.3497704e.js
+++
@@ -1,8 +0,0 @@
-import{_ as e,c as t,o,a as n}from"./app.510757aa.js";const f='{"title":".gitignore \u6587\u4EF6 Gitignore file","description":"","frontmatter":{},"relativePath":"directory-structure/gitignore.md","lastUpdated":1638489737476}',r={},i=n(`<h1 id="gitignore-\u6587\u4EF6-gitignore-file" tabindex="-1"><code>.gitignore</code> \u6587\u4EF6 <a href="https://v3.nuxtjs.org/docs/directory-structure/gitignore" target="_blank" rel="noopener noreferrer">Gitignore file</a> <a class="header-anchor" href="#gitignore-\u6587\u4EF6-gitignore-file" aria-hidden="true">#</a></h1><p>\u901A\u8FC7 <code>.gitignore</code> \u6587\u4EF6\u6307\u5B9A git \u5E94\u8BE5\u5FFD\u7565\u7684\u6545\u610F\u53D6\u6D88\u8DDF\u8E2A\u7684\u6587\u4EF6\u3002\u5728 <a href="https://git-scm.com/docs/gitignore" target="_blank" rel="noopener noreferrer">git \u6587\u6863</a> \u4E2D\u4E86\u89E3\u66F4\u591A\u4FE1\u606F\u3002</p><p>\u6211\u4EEC\u5EFA\u8BAE\u4F7F\u7528\u4E00\u4E2A <code>.gitignore</code> \u6587\u4EF6\uFF0C\u8BE5\u6587\u4EF6<strong>\u81F3\u5C11</strong>\u5305\u542B\u4EE5\u4E0B\u6761\u76EE\uFF1A</p><div class="language-bash"><pre><code><span class="token comment"># Nuxt dev/build \u8F93\u51FA</span>
-.output
-.nuxt
-<span class="token comment"># Node \u4F9D\u8D56</span>
-node_modules
-<span class="token comment"># \u7CFB\u7EDF\u6587\u4EF6</span>
-*.log
-</code></pre></div>`,4),a=[i];function s(c,g,d,p,_,l){return o(),t("div",null,a)}var h=e(r,[["render",s]]);export{f as __pageData,h as default};
